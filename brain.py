4c33d04b1c597aa8882c33bae1cfc8dff0f91441
passap <passapungchai@gmail.com>
2015-05-04 09:42:56 -0700

    Added start func,generated first generation of cells.

--- brain.py
+++ brain.py
@@ -1,59 +1,54 @@
 from graphics import *
 import math
 
-a = 50 #the top corner coords. 
-b = 70 #the second bottom corner. 
+a = 50 #the top corner coords.
+b = 70 #the second bottom corner.
 length = b - a
 window_width = 800
 window_height = 600
 row_total = (window_width - (2 * a)) / length
-cell_total = ((window_width - 20) * (window_height - 20)) / (length * length)
+col_total = (window_height - (2 * a)) / length
+cell_total = row_total * col_total
 #the cell class contains methods to init state, change state
-#coordinates as well. 
+#coordinates as well.
 
 class Cell:
 
     stat = 0
     color = 'white'
     def change(self):
-        if self.stat == 0:  # 0 is dead, 1 is alive. Start off dead, all of them. 
+        if self.stat == 0:  # 0 is dead, 1 is alive. Start off dead, all of them.
             self.stat = 1
             self.color = 'blue2'
-        else: 
+            self.sqr.setFill(self.color)
+        else:
             self.stat = 0
             self.color = 'white'
+            self.sqr.setFill(self.color)
 
-    def loc(self, num): 
-        if num == 0: 
+    def loc(self, num):
+        if num == 0:
             self.FC = Point(a,a)
             self.BC = Point(b,b)
             self.col = 0
-            self.row_num = 0 
+            self.row_num = 0
             self.sqr = Rectangle(self.FC,self.BC)
-        else: 
-            self.col = num % row_total #position in row. 
+        else:
+            self.col = num % row_total #position in row.
             self.row_num = math.trunc(num / row_total)
-            self.fA = a + (self.col * length) #the distance across 
+            self.fA = a + (self.col * length) #the distance across
             self.fU = a + (self.row_num * length) #the distance up & down
             self.FC = Point(self.fA, self.fU) #the first corner
-            self.BC = Point(self.fA + length, self.fU + length) #the second corner 
-            self.sqr = Rectangle(self.FC, self.BC) 
+            self.BC = Point(self.fA + length, self.fU + length) #the second corner
+            self.sqr = Rectangle(self.FC, self.BC)
             self.sqr.setFill(self.color)
 
-
-
-box = []
-
-
-def gridInit(): 
+def gridInit(case):
     screen = GraphWin("Conway's Game of Life", window_width, window_height, autoflush = False)
-    update()
-    for x in range(0, cell_total): 
+    for x in range(0, cell_total):
         j = Cell()
         j.loc(x)
-        box.append(j)
-        box[x].sqr.draw(screen)
+        case.append(j)
+        case[x].sqr.draw(screen)
         update()
-    screen.getMouse() # pause for click in window
-    screen.close()
-
+    return screen
--- checker.py
+++ checker.py
@@ -1,4 +1,22 @@
-from brain import * 
+from brain import *
 from graphics import *
 import math
-import time 
+import time
+import random
+
+box = []
+
+def start(board):
+    numAlive = random.randint(1,cell_total) #random number of cells
+    for x in range(0, numAlive):
+        cellMk = random.randint(0,cell_total - 1)
+        box[cellMk].change()
+        board.flush()
+        update()
+    return 0
+
+board = gridInit(box) #making that grid.
+board.getMouse()
+start(board)
+board.getMouse()
+board.close()
